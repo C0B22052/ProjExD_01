b6b15e58927a819c1ca3df3d84cf43824bbe2b76
kinjo kose <c0b220522d@edu.teu.ac.jp>
2023-04-18 17:50:59 +0900

    練習問題3

--- pygame_image.py
+++ pygame_image.py
@@ -7,10 +7,10 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01-20230418/fig/pg_bg.jpg")
     kokaton3 = pg.image.load("ex01-20230418/fig/3.png") #練習問題2
-    
+    kokaton3 = pg.transform.flip(kokaton3, True, False)
+    kokaton3s = [kokaton3,pg.transform.rotozoom(kokaton3,10,1.0)] #練習問題3
 
     tmr = 0
-
 
     while True:
         for event in pg.event.get():
@@ -21,7 +21,6 @@ def main():
         screen.blit(kokaton3,[0,0])
         pg.display.update()
         clock.tick(100)
-
 
 if __name__ == "__main__":
     pg.init()
